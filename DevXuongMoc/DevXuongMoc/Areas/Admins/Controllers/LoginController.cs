bedb2ee540be38402fc3bc71408655c1169da97d
Sangnguyen3110 <nguyenhuusang31102002@gmail.com>
2025-03-23 23:24:40 +0700

    23/3/2025

---
+++ DevXuongMoc/DevXuongMoc/Areas/Admins/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using Devxuongmoc.Areas.Admins.Models;
+using DevXuongMoc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevXuongMoc.Areas.Admins.Controllers
+{
+    [Area("Admins")]
+    public class LoginController : Controller
+    {
+        public DevXuongMocContext _context;
+
+        public LoginController(DevXuongMocContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]// get, hiển thị form để nhập dữ liệu
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost] // POST -> khi submit form
+        public IActionResult Index(Login model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError(string.Empty, "Thông tin đăng nhập không hợp lệ.");
+                return View(model);
+            }
+
+            var pass = model.Password;
+            var dataLogin = _context.AdminUsers.FirstOrDefault(x => x.Email.Equals(model.Email) && x.Password.Equals(pass));
+            if (dataLogin != null)
+            {
+                HttpContext.Session.SetString("AdminLogin", model.Email);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Thông tin đăng nhập không chính xác.");
+                return View(model);
+            }
+
+        }
+        [HttpGet]// thoát đăng nhập, huỷ session
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("AdminLogin"); // huỷ session với key AdminLogin đã lưu trước đó
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DevXuongMoc/DevXuongMoc/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using DevXuongMoc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevXuongMoc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DevXuongMocContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, DevXuongMocContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var hotProducts = _context.Products.Where(p => p.Hot == 1).ToList();
+
+            // Truyền danh sách này vào View
+            return View(hotProducts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
